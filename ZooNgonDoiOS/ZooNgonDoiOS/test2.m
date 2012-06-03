d34eab645f35878bdfadf78552f50a53c91c15c7
Tran Minh Long <tranminhlong177@yahoo.com>
2012-06-03 17:43:43 +0700

    abc

---
+++ ZooNgonDoiOS/ZooNgonDoiOS/test2.m
@@ -0,0 +1,31 @@
+//
+//  test2.m
+//  ZooNgonDoiOS
+//
+//  Created by Tran Minh Long on 3/6/12.
+//  Copyright (c) 2012 Personal. All rights reserved.
+//
+
+#import "test2.h"
+
+@implementation test2
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end

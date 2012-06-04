c570e57f3617f11ce1828adf8954895c2b8a8b54
Tran Minh Long <tranminhlong177@yahoo.com>
2012-06-04 22:45:30 +0700

    Add some constants string

---
+++ ZooNgonDoiOS/ZooNgonDoiOS/Constants.h
@@ -0,0 +1,20 @@
+//
+//  Common.h
+//  ZooNgonDoiOS
+//
+//  Created by Tran Minh Long on 4/6/12.
+//  Copyright (c) 2012 Personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//File path
+#define BackgroundImageConstants @"general_grass_bg_pattern.png"
+#define BackgroundFormLoginConstants @"general_paper_bg.png"
+
+//Text
+#define UsernameTextConstants @"Tên đăng nhập"
+#define PasswordTextConstants @"Mật khẩu"
+
+@interface Constants : NSObject
+@end
---
+++ ZooNgonDoiOS/ZooNgonDoiOS/Constants.m
@@ -0,0 +1,12 @@
+//
+//  Common.m
+//  ZooNgonDoiOS
+//
+//  Created by Tran Minh Long on 4/6/12.
+//  Copyright (c) 2012 Personal. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+@end
